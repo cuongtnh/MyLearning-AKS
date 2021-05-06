542d7f1901ea6bd37b2b9296dab8352efd95c650
Cuong Tran <67938422+cuongtnh@users.noreply.github.com>
2021-05-06 21:38:40 +0700

    Add files via upload

---
+++ PostgresSQL-deployment.tf
@@ -0,0 +1,49 @@
+resource "kubernetes_deployment" "postgres" {
+  metadata {
+    name = "scalablepostgres"
+    labels = {
+      App = "scalablepostgres"
+    }
+  }
+
+  spec {
+    replicas = 1 
+    selector {
+      match_labels = {
+        App = "scalablepostgres"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "scalablepostgres"
+        }
+      }
+      spec {
+        container {
+          image = "postgres:latest"
+          name  = "postgres"
+
+          port {
+            container_port = 5432
+          }
+          env_from {
+            config_map_ref {
+              name = "postgresconfigmap"
+            }
+          }
+          volume_mount {
+            mount_path = "/var/lib/postgresql/data"
+            name = "postgresdisk01"
+          }
+        }
+        volume {
+          name = "postgresdisk01"
+          empty_dir {
+            medium = ""
+          }
+        }
+      }
+    }
+  }
+}
